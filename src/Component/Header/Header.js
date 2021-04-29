281229b1e61ef9c567abb95a4190108cef10bb2e
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2021-04-29 11:47:00 +0100

    created project with create react app and added files for home

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+export default function Products() {
+    return (
+        <div className="flexbox-container">
+            <p className="logo">
+                <span>Logo</span>
+            </p>   
+            <nav>
+                <p>Cart</p>
+            </nav> 
+        </div>
+    )
+}
---
+++ src/Component/ProductList/Products.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+ class Products extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    <ul>
+                        <li>Shirt</li>
+                        <li>Trousers</li>
+                        <li>Coat</li>
+                        <li>Shoe</li>
+                    </ul>
+                </p>
+            </div>
+        )
+    }
+}
+export default Products;
+
