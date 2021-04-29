0e5dc1798144473232c7193454411e8edea6799d
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2021-04-29 16:11:26 +0100

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -11,29 +11,34 @@ class Directory extends React.Component {
             {
              title:'hats',
              imageUrl:'https://images.pexels.com/photos/270640/pexels-photo-270640.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-             id:1
+             id:1,
+             linkUrl:'hats'
             },
             {
                 title:'jackets',
                 imageUrl:'https://images.pexels.com/photos/5882683/pexels-photo-5882683.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                id:2
+                id:2,
+                linkUrl:''
             },
             {
                 title:'sneakers',
                 imageUrl:'https://images.pexels.com/photos/461082/pexels-photo-461082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-                id:3
+                id:3,
+                linkUrl:''
             },
             {
                 title:'womens',
                 imageUrl:'https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
                 size:'large',
-                id:4
+                id:4,
+                linkUrl:''
             },
             {
                 title:'mens',
                 imageUrl:'https://images.pexels.com/photos/5053849/pexels-photo-5053849.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
                 size:'large',
-                id:5
+                id:5,
+                linkUrl:''
             }]
     }
     
@@ -41,11 +46,11 @@ class Directory extends React.Component {
 render(){
     return(
       <div className='directory-menu'>
-          {this.state.sections.map(({title, imageUrl, id,size})=>(
-                  <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+          {this.state.sections.map(({id, ...otherSectionProps })=>(
+                  <MenuItem key={id} {...otherSectionProps}/>
               ))}
       </div>  
     );
- }
+ }  
 }
 export default Directory;
